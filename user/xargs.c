f96479609d74b9048841116076fc876c1ec3a8c9
zhige <1002515712@qq.com>
2025-03-13 11:26:58 +0800

    完成xargs

---
+++ user/xargs.c
@@ -0,0 +1,56 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "kernel/fs.h"
+#include "user/user.h"
+
+void run(char *program, char **args) {
+	if(fork() == 0) {
+		exec(program, args);
+		exit(0);
+	}
+	return;
+}
+
+
+int main(int argc, char *argv[]) {
+	char buf[2048];
+	char *p = buf, *last_p = buf;
+	char *argsbuf[128];
+	char **args = argsbuf;
+
+	for(int i = 1; i < argc; i++) {
+		*args = argv[i];
+		args++;
+	}
+
+	char **pa = args;
+
+	while(read(0, p, 1) != 0) {
+		if(*p == ' ' || *p == '\n') {
+			char temp = *p;
+			*p = '\0';
+
+			*(pa++) = last_p;
+			last_p = p + 1;
+
+			if(temp == '\n') {
+				*pa = 0;
+				run(argv[1], argsbuf);
+				pa = args;
+			}
+		}
+		p++;
+	}
+
+	if(pa != args) {
+		*p = '\0';
+		*(pa++) = last_p;
+		*pa = 0;
+		
+		run(argv[1], argsbuf);
+	}
+
+	while(wait(0) != -1) ;
+
+	exit(0);
+}
